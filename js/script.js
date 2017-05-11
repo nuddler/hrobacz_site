53089f17b79081b173d22625d530d52bdb84f45f
Bartłomiej Długosz <you@example.com>
2017-05-11 12:47:33 +0200

    Porządki w kodzie

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+$(document).ready(function(e) {
+	$('.with-hover-text, .regular-link').click(function(e){
+		e.stopPropagation();
+	});
+	
+	/***************
+	* = Hover text *
+	* Hover text for the last slide
+	***************/
+	$('.with-hover-text').hover(
+		function(e) {
+			$(this).css('overflow', 'visible');
+			$(this).find('.hover-text')
+				.show()
+				.css('opacity', 0)
+				.delay(200)
+				.animate(
+					{
+						paddingTop: '25px',
+						opacity: 1
+					},
+					'fast',
+					'linear'
+				);
+		},
+		function(e) {
+			var obj = $(this);
+			$(this).find('.hover-text')
+				.animate(
+					{
+						paddingTop: '0',
+						opacity: 0
+					},
+					'fast',
+					'linear',
+					function() {
+						$(this).hide();
+						$( obj ).css('overflow', 'hidden');
+					}
+				);
+		}
+	);	
+
+	        $(document).ready(function(e) { 
+            var lis = $('.nav > li');
+            menu_focus(lis[0], 1);
+
+            $(".fancybox").fancybox({
+                padding : 10,
+                helpers : {
+                    overlay : {
+                        locked : false
+                    }
+                }
+            });
+
+        });
+
+});
